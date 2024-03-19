e9f311fbb50e5fa021239894b7fa7affe44bc338
Prem <premmodhaofficial@gmail.com>
2024-03-19 10:19:31 +0530

    added db?

--- Index/models.py
+++ Index/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Note(models.Model):
+    note_name = models.CharField(max_length=30)
+    pub_date = models.DateTimeField('date added')
+    note_file = models.FileField(upload_to='./uploaded_files')
+
+    def __str__(self):
+        return f'{self.note_name}: {self.pub_date}'
